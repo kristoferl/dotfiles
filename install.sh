50b030bf671463e937509b0d3b2687eb3455b6e0
Kristofer Löfgren <kristofer.lovgren@gmail.com>
2016-08-18 10:19:47 +0200

    Update the .vim symlink creation. The gitconfig_user files is copied instead of symlinked.

--- install.sh
+++ install.sh
@@ -8,11 +8,23 @@ for f in $DIR/rc/*; do
   ln -fs "$f" ".$(basename "$f")"
 done
 
-# Save old .vim dir before replacing it
+# If .vim exists and is a symlink, remove the link
+# If .vim exists and is a directory, remove it
 cd ~ || exit
-mv .vim .vim.bak
-ln -fs "$DIR/vim" ".vim"
+if [[ -L ~/.vim ]]; then
+  # echo ".vim is a Symlink"
+  rm ~/.vim
+elif [[ -d ~/.vim ]]; then
+  # echo ".vim is dir"
+  mv ~/.vim ~/.vim.bak
+fi
+cd ~ || exit
+# TODO: Copy directly to .vim
+ln -fs "$DIR/vim"
+mv ~/vim ~/.vim
 
+# Copy user specific git config file
+cp "$DIR/files_to_copy/gitconfig_user" ~/.gitconfig_user
 
 # Symlink files in the fish folder
 mkdir -p ~/.config/fish
