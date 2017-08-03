06f04e8e523995eccd8bfeed2a31a30f97419ab9
Kristofer Löfgren Patrakka <kristofer.lovgren@gmail.com>
2017-08-03 09:22:07 +0200

    Use https://github.com/jseb/cask-upgrade to upgrade casks

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -13,28 +13,14 @@ function update --description 'Runs various update & upgrade commands'
   #brew doctor
 
   echo "# Upgrading apm"
-  #echo " >apm update"
-  #apm update
   echo " >apm upgrade"
   apm upgrade --no-confirm
   echo " >apm clean"
   apm clean
 
   echo "# Upgrading cask"
-  echo " >brew cask update"
-  #brew cask update
-  # Workaround to update casks
-  # Silenced error messages when brew cask installing
-  #echo "ATT! Cask upgrade is disabled at the moment. Unclear how to do it then 'brew cask update' should no longer be used"
-  set -l CASKLIST (brew cask list 2>/dev/null)
-  for cask in $CASKLIST
-  # Here it might be better to use 'brew cask reinstall'?
-    echo "    > Installing $cask"
-    brew cask install $cask 2>/dev/null
-  end
-  echo " >brew cask cleanup"
-  brew cask cleanup
-
+  # https://github.com/jseb/cask-upgrade
+  cask-upgrade -n
 
   echo "# Update fish completions"
   fish -c "fish_update_completions"
