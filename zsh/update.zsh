4ac1cbb4e6802074a7827f37b3565f4dece78dd2
Kristofer Löfgren Patrakka <1071958+kristoferl@users.noreply.github.com>
2025-09-26 10:04:25 +0200

    Update update script

--- zsh/update.zsh
+++ zsh/update.zsh
@@ -1,7 +1,6 @@
 update () {
   echo "Update oh-my-zsh"
-  # Instead of running "omz update", run "zsh -i -c 'omz update'", this is because if an update i applied, the shell is restarted and the function is aborted.
-  zsh -i -c 'omz update'
+  $ZSH/tools/upgrade.sh
   echo "# Upgrading brew"
   echo " >brew update"
   brew update
@@ -10,16 +9,7 @@ update () {
   echo " >brew cleanup"
   brew cleanup -s
   #echo " >brew doctor"
-  #brew doctor
-
-  echo "# Upgrading apm"
-  echo " >apm upgrade"
-  apm upgrade --no-confirm
-  echo " >apm clean"
-  apm clean
-
-  echo "# Upgrading cask"
-  brew upgrade --cask
+  brew doctor
 
   echo "# All done"
 }
